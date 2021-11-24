a29528245a78b273076a912cabbdf844a2e80712
leereyno <leebreynolds@gmail.com>
2021-11-24 13:44:45 -0500

    group fix
    Signed-off-by: leereyno <leebreynolds@gmail.com>

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,13 +6,24 @@ node default {
   #    owner   => 'root',
   #  }
 
-  package { 'gcc.x86_64':
-    ensure => present,
+  # Yum stuff
+  # https://forge.puppet.com/modules/puppet/yum
+
+  # Only allow two kernel releases on the system at any given time
+  yum::config { 'installonly_limit':
+    ensure => 2,
   }
 
-  yum::group { 'development','additional-devel':
+  yum::group { 'development':
     ensure  => present,
     timeout => 300,
   }
+
+  yum::group { 'additional-devel':
+    ensure  => present,
+    timeout => 300,
+  }
+
+
 }
 
