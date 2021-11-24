40998586121954ba9742ecbb471b2fdf2f3016a9
leereyno <leebreynolds@gmail.com>
2021-11-24 15:48:09 -0500

    add master.puppet.vm
    Signed-off-by: leereyno <leebreynolds@gmail.com>

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,28 +5,29 @@ node default {
   #    content => 'This is a readme',
   #    owner   => 'root',
   #  }
-
-  # Yum stuff
-  # https://forge.puppet.com/modules/puppet/yum
-
-  # The line below does not work...needs study
-  #  mod 'puppet-yum', '5.3.0'
-
-  # Only allow two kernel releases on the system at any given time
-  yum::config { 'installonly_limit':
-    ensure => 2,
-  }
-
-  yum::group { 'development':
-    ensure  => present,
-    timeout => 300,
-  }
-
-  yum::group { 'additional-devel':
-    ensure  => present,
-    timeout => 600,
-  }
-
-
+  #
+  #  # Yum stuff
+  #  # https://forge.puppet.com/modules/puppet/yum
+  #
+  #  # Only allow two kernel releases on the system at any given time
+  #  yum::config { 'installonly_limit':
+  #    ensure => 2,
+  #  }
+  #
+  #  yum::group { 'development':
+  #    ensure  => present,
+  #    timeout => 300,
+  #  }
+  #
+  #  yum::group { 'additional-devel':
+  #    ensure  => present,
+  #    timeout => 600,
+  #  }
+  #
 }
+
+node 'master.puppet.vm' {
+  include role::master_server
+}
+
 
