0391ef69b9cd1a17eea5fe52514d9460c40406fb
leereyno <leebreynolds@gmail.com>
2021-11-29 11:12:11 -0500

    Puppet ordering
    Signed-off-by: leereyno <leebreynolds@gmail.com>

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -7,6 +7,7 @@ class minecraft {
   file {'/opt/minecraft/server.jar':
     ensure => file,
     source => 'http://192.168.1.10/minecraft/server.jar',
+    before Service['minecraft'],
   }
 
   file {'/etc/yum.repos.d/epel.repo':
@@ -29,8 +30,9 @@ class minecraft {
   }
 
   service {'minecraft':
-    ensure => running,
-    enable => true,
+    ensure  => running,
+    enable  => true,
+    require => [Package['java-latest-openjdk-17.0.1.0.12-1.rolling.el7.x86_64'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service']]
   }
 
 }
